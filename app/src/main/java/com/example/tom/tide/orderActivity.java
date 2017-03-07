be6fe12a78b41138f9dbd29f299660a94f1b9242
TOM <tom710236@gmail.com>
2017-03-07 09:19:28 +0800

    檢貨功能1.0

--- app/src/main/java/com/example/tom/tide/orderActivity.java
+++ app/src/main/java/com/example/tom/tide/orderActivity.java
@@ -1,16 +1,24 @@
 package com.example.tom.tide;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AbsListView;
 import android.widget.ArrayAdapter;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -30,28 +38,52 @@ import okhttp3.Response;
 public class orderActivity extends AppCompatActivity {
     String check, door1, cUserName;
     String url = "http://demo.shinda.com.tw/ModernWebApi/WebApi.aspx";
-    OkHttpClient client = new OkHttpClient();
     String check2 = null;
     String check3 = null;
-    String cProductName;
-    String cProductID;
-    String cShippersCount;
-    int cShippersCountEd;
     String json;
+    int addNum=0;
+    LinearLayout linear;
+
+    ArrayAdapter<ProductInfo> list;
+    ArrayList<ProductInfo> trans;
+
+    OkHttpClient client = new OkHttpClient();
+
     EditText editText;
-    String order;
-    ArrayAdapter<String> list, list2;
-    int a = 0;
-    int myint = 0;
-    ArrayList<String> trans, trans2;
     ListView listView;
-    int number;
+
+    public class ProductInfo
+    {
+        private String mProductName;
+        private String mProductID;
+        private int mProductCount=0;
+        private int mStocks = 0;
+
+        ProductInfo(final String productName, final String productID, int productCount, int cShippersCountEd)
+        {
+            this.mProductName = productName;
+            this.mProductID = productID;
+            this.mProductCount = productCount;
+
+        }
+
+        @Override
+        public String toString()
+        {
+            return this.mProductName + "(" + this.mProductID + ") " + this.mProductCount + " (" + this.mStocks + ")";
+        }
+    }
+
+
+
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
+        //getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
 
         //接收上一頁的資料
         Intent intent = getIntent();
@@ -65,7 +97,10 @@ public class orderActivity extends AppCompatActivity {
         //再取字串範圍 (0和最後是[])
         //取代
         check2 = check.substring(1, i - 1);
+        //check3 = check2.replaceAll(", ", ",");
         check3 = check2.replaceAll(", ", ",");
+        Log.e("check3", "check3: " + check3);
+
 
         TextView orderName = (TextView) findViewById(R.id.textView11);
         //把上一頁傳過來的door用TextView顯示
@@ -97,8 +132,40 @@ public class orderActivity extends AppCompatActivity {
                 postjson();
             }
         }).start();
+        editText = (EditText) findViewById(R.id.editText);
+        trans = new ArrayList<ProductInfo>();
+        listView = (ListView) findViewById(R.id.list);
+        listView.setChoiceMode(AbsListView.CHOICE_MODE_SINGLE);
+        list = new ArrayAdapter<>(orderActivity.this, android.R.layout.simple_list_item_activated_1, trans);
+
+        Switch sw = (Switch)findViewById(R.id.switch2);
+        sw.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+               if(isChecked){
+                   linear = (LinearLayout)findViewById(R.id.linear);
+                   linear.setVisibility(View.VISIBLE);
+                   addNum=1;
+               }
+                else{
+                   linear.setVisibility(View.INVISIBLE);
+                   addNum=0;
+               }
+
+            }
+        });
 
 
+    }
+    public void add1 (View v){
+        addNum=1;
+    }
+    public void add3 (View v){
+        addNum=3;
+    }
+    public void add5 (View v){
+        addNum=5;
     }
 
     //執行執行緒的方法
@@ -120,7 +187,7 @@ public class orderActivity extends AppCompatActivity {
 
             @Override
             public void onResponse(Call call, Response response) throws IOException {
-//POST後 回傳的JSON檔
+                //POST後 回傳的JSON檔
                 json = response.body().string();
                 Log.e("json",json);
                 Log.e("OkHttp10", response.toString());
@@ -134,105 +201,117 @@ public class orderActivity extends AppCompatActivity {
     //取出回傳後JSON的值
     private void parseJson2(String json) {
         try {
-            trans = new ArrayList<String>();
             final JSONArray array = new JSONArray(json);
-            for (int i = 0; i < array.length(); i++) {
+            for (int i = 0; i < array.length(); i++)
+            {
                 JSONObject obj = array.getJSONObject(i);
-                cProductName = obj.getString("cProductName");
-                cProductID = obj.getString("cProductID");
-                cShippersCount = obj.getString("cShippersCount");
-                int cShippersCountEd = obj.getInt("cShippersCountEd");
-                Log.e("okHTTP15", cProductName + cProductID + cShippersCount);
-                trans.add(cProductName + "(" + cProductID + ")" + cShippersCount + "(" + cShippersCountEd + ")");
-
-
+                trans.add(new ProductInfo(obj.optString("cProductName"), obj.optString("cProductID"),obj.optInt("cShippersCount"),obj.optInt("cShippersCountEd")));
+                Log.e("trans", String.valueOf(trans));
             }
-//設定ListView
-            listView = (ListView) findViewById(R.id.list);
-            listView.setChoiceMode(AbsListView.CHOICE_MODE_MULTIPLE);
-            list = new ArrayAdapter<>(
-                    orderActivity.this,
-                    android.R.layout.simple_list_item_activated_1,
-                    trans);
-
-            //顯示ListView 非主執行緒的UI
-            runOnUiThread(new Runnable() {
-
+            runOnUiThread(new Runnable()
+            {
                 @Override
-                public void run() {
-//listView.setVisibility(View.VISIBLE);
+                public void run()
+                {
                     listView.setAdapter(list);
+                    Log.e("list", String.valueOf(list));
                 }
             });
 
+        } catch (JSONException e1) {
+            e1.printStackTrace();
+        }
 
-        } catch (JSONException e) {
-            e.printStackTrace();
+    }
+
+    public void enter(View v)
+    {
+        final String UserEnterKey = editText.getText().toString();
+        final ProductInfo product = getProduct(UserEnterKey);
+
+        if (product != null && product.mStocks>=0)
+        {
+            if(addNum==1){
+                if(product.mStocks>product.mProductCount||product.mStocks+addNum>product.mProductCount){
+                    product.mStocks=product.mProductCount;
+                    list.notifyDataSetChanged();
+                    Toast.makeText(orderActivity.this,"數量已滿", Toast.LENGTH_SHORT).show();
+                }else{
+                    product.mStocks=product.mStocks+addNum;
+                    list.notifyDataSetChanged();
+                }
+
+            }
+            else if(addNum==3){
+                if(product.mStocks>product.mProductCount||product.mStocks+addNum>product.mProductCount){
+                    product.mStocks=product.mProductCount;
+                    list.notifyDataSetChanged();
+                    Toast.makeText(orderActivity.this,"數量已滿", Toast.LENGTH_SHORT).show();
+                }else{
+                    product.mStocks=product.mStocks+addNum;
+                    list.notifyDataSetChanged();
+                }
+            }
+            else if(addNum==5){
+                if(product.mStocks>product.mProductCount||product.mStocks+addNum>product.mProductCount){
+                    product.mStocks=product.mProductCount;
+                    list.notifyDataSetChanged();
+                    Toast.makeText(orderActivity.this,"數量已滿", Toast.LENGTH_SHORT).show();
+                }else{
+                    product.mStocks=product.mStocks+addNum;
+                    list.notifyDataSetChanged();
+                }
+        }
+            else if(addNum==0){
+
+                final View item = LayoutInflater.from(orderActivity.this).inflate(R.layout.activity_alertdialog, null);
+                new AlertDialog.Builder(orderActivity.this)
+                        .setTitle("請輸入數量")
+                        .setView(item)
+                        .setNegativeButton("取消", null)
+                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                EditText editText = (EditText) item.findViewById(R.id.editText2);
+                                if(editText.length()!=0){
+                                    int addnum = Integer.parseInt(editText.getText().toString());
+                                    if(addnum >product.mProductCount||product.mStocks+addnum>product.mProductCount||product.mStocks>product.mProductCount){
+                                        Log.e("addnum", String.valueOf(addnum));
+                                        product.mStocks=product.mProductCount;
+                                        list.notifyDataSetChanged();
+                                        Toast.makeText(orderActivity.this,"數量已滿", Toast.LENGTH_SHORT).show();
+
+                                    }else{
+                                        product.mStocks=product.mStocks+addnum;
+                                        list.notifyDataSetChanged();
+                                    }
+                                }else{
+                                    Toast.makeText(orderActivity.this,"請輸入數量", Toast.LENGTH_SHORT).show();
+                                }
+
+
+                            }
+                        })
+                        .show();
+            }
+
+        } else
+        {
+            Toast.makeText(orderActivity.this,"請輸入正確商品條碼", Toast.LENGTH_SHORT).show();
         }
     }
 
-    public void enter(View v) throws JSONException {
-
-        editText = (EditText) findViewById(R.id.editText);
-        order = editText.getText().toString();
-        trans2 = new ArrayList<String>();
-        final JSONArray array = new JSONArray(json);
-        for (int i = 0; i < array.length(); i++) {
-            JSONObject obj = array.getJSONObject(i);
-            Log.e("OBJ", String.valueOf(obj));
-            cProductName = obj.getString("cProductName");
-            cProductID = obj.getString("cProductID");
-            cShippersCount = obj.getString("cShippersCount");
-            int cShippersCountEd2 = obj.getInt("cShippersCountEd");
-            Log.e("cShippersCountEd3", String.valueOf(cShippersCountEd2));
-            Log.e("trans2之前", String.valueOf(trans2));
-            trans2.add(cProductName + "(" + cProductID + ")" + cShippersCount + "(" + cShippersCountEd + ")");
-            Log.e("trans2之後", String.valueOf(trans2));
-            if (order.equals(cProductID)) {
-                Log.e("成功", String.valueOf(array));
-                //第二次cShippersCountEd 因為數量改變 所以找不到索引值
-                int k = trans2.indexOf(cProductName + "(" + cProductID + ")" + cShippersCount + "(" + cShippersCountEd + ")");
-                //int k = trans.indexOf(cProductID);
-                //Log.e("trans2對比後", String.valueOf(trans2));
-                Log.e("比對後索引", String.valueOf(k));
-                //這邊加數字 有問題
-                cShippersCountEd2++;
-                Log.e("cShippersCountEd", String.valueOf(cShippersCountEd));
-                Log.e("cShippersCountEd2", String.valueOf(cShippersCountEd2));
-                //trans.remove(k);
-                trans.set(k, cProductName + "(" + cProductID + ")" + cShippersCount + "(" + cShippersCountEd2 + ")");
-                trans2.set(k, cProductName + "(" + cProductID + ")" + cShippersCount + "(" + cShippersCountEd2 + ")");
-                //Log.e("trans", String.valueOf(trans));
-                //k = trans.indexOf(cProductName + "(" + cProductID + ")" + cShippersCount + "(" + cShippersCountEd + ")");
-
-                //Log.e("int2", String.valueOf(k));
-                runOnUiThread(new Runnable() {
-
-                    @Override
-                    public void run() {
-
-
-                        //list.notifyDataSetChanged();
-                        listView.setAdapter(list);
-                        Log.e("listview", String.valueOf(listView));
-                    }
-                });
-
-
-
-
-
-            } else {
-
-                Log.e("比對失敗", String.valueOf(trans));
-            }
-
-
-
+    private ProductInfo getProduct(final String key)
+    {
+        if (TextUtils.isEmpty(key))
+            return null;
+        for (int index = 0; index < trans.size(); index++)
+        {
+            ProductInfo product = trans.get(index);
+            if (product.mProductID.equals(key))
+                return product;
         }
-
-
-
+        return null;
     }
 }
 
