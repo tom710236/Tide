00ac8cc6982d87f6d0222f580129517373ad015b
tom710236 <tom710236@gmail.com>
2017-03-19 17:42:43 +0800

    註解完畢

--- app/src/main/java/com/example/tom/tide/MainActivity.java
+++ app/src/main/java/com/example/tom/tide/MainActivity.java
@@ -21,7 +21,7 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
-
+//0319
 public class MainActivity extends AppCompatActivity {
     //宣告
     String cStatus, userName, passWord,cUserName;
